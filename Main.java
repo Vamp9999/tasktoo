afb2d9d8b3c4b182cd3226fda89fe6346cd49e7e
Vamp9999 <venimkoki1@gmail.com>
2025-04-16 20:10:48 +0200

    code that Read XML and Output User-Selected Fields

--- Main.java
+++ Main.java
@@ -1,31 +1,38 @@
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            File xmlFile = new File("data.xml"); // place your XML file in project folder
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter field(s) to display (comma separated): ");
+            String[] selectedFields = input.nextLine().split(",");
+
+            for (int i = 0; i < selectedFields.length; i++) {
+                selectedFields[i] = selectedFields[i].trim();
+            }
+
+            File xmlFile = new File("data.xml");
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(xmlFile);
             doc.getDocumentElement().normalize();
 
-            NodeList nodeList = doc.getElementsByTagName("user"); // or relevant XML tag
+            NodeList users = doc.getElementsByTagName("user");
 
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
-                System.out.println("User " + (i + 1));
-                if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    NodeList children = element.getChildNodes();
-                    for (int j = 0; j < children.getLength(); j++) {
-                        Node child = children.item(j);
-                        if (child.getNodeType() == Node.ELEMENT_NODE) {
-                            System.out.println(child.getNodeName() + ": " + child.getTextContent());
-                        }
+            for (int i = 0; i < users.getLength(); i++) {
+                Element user = (Element) users.item(i);
+                System.out.println("User " + (i + 1) + ":");
+                for (String field : selectedFields) {
+                    NodeList fieldNode = user.getElementsByTagName(field);
+                    if (fieldNode.getLength() > 0) {
+                        System.out.println(field + ": " + fieldNode.item(0).getTextContent());
+                    } else {
+                        System.out.println(field + ": [Not found]");
                     }
                 }
+                System.out.println();
             }
         } catch (Exception e) {
             e.printStackTrace();
